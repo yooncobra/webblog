1a16509cbf67a666661d87e97dbbb117d15b09e7
yooncobra <yooncobra@gmail.com>
2016-04-11 13:42:17 +0900

    blog.views.post_list 뷰를 구현: Post 모델에서 데이터를 가져와서, 템플릿에 뿌려주기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from blog.models import Post
 
 def index(request):
     return render(request, 'blog/index.html')
+
+
+def post_list(request):
+    post_list = Post.objects.all()
+    params = {'post_list': post_list}
+    return render(request, 'blog/post_list.html', params)
     
