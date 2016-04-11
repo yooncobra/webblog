292f74cdf765c611aa30d5682a5c902b96556879
yooncobra <yooncobra@gmail.com>
2016-04-11 13:13:48 +0900

    기본 admin으로는 성에 차지 않는다. admin Post list 부분만 커스터마이징 해보자.

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from blog.models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'get_title_length']
+    list_display_links = ['title']
+    
+    def get_title_length(self, post):
+        return len(post.title)
+
+admin.site.register(Post, PostAdmin)
+
