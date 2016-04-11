f6bcaed1d6139a2247ec9256dde17749980bdda2
yooncobra <yooncobra@gmail.com>
2016-04-11 12:33:56 +0900

    blog/models.py 에 Post모델 등록-> DB에 반영하려면 migration해 줘야 한다.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, help_text='포스팅 제목을 100자 이내로 써 주세요.')
+    content = models.TextField()
+    photo = models.ImageField(blank=True, help_text='이미지를 첨부하세요.')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
