c53c3e3e3122f788d3ee08e2e271e8ad3ce46a62
yooncobra <yooncobra@gmail.com>
2016-05-30 17:11:41 +0900

    blog.models.Post와 Tag의 M:N관계로 구현. blank=True 옵션으로 '빈 관계'에 대해서도 허용

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100, help_text='포스팅 제목을 100자 이내로 써 주세요.')
     content = models.TextField()
     photo = models.ImageField(blank=True, help_text='이미지를 첨부하세요.')
+    tags = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
@@ -21,4 +22,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.message
+
+
+@python_2_unicode_compatible
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
