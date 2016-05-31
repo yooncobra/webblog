da47fa53125d6227a22412098ee39f4971291bba
yooncobra <yooncobra@gmail.com>
2016-05-31 22:44:25 +0900

    회원가입 시에, email 을 추가로 입력받도록 함

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,20 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import get_user_model
+
+
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        # fields = ['username', 'email']
+        fields = UserCreationForm.Meta.fields + ('email',)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', None)
+        if email:
+            User = get_user_model()
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError('중복된 이메일')
+        return email
 
 
 class LoginForm(AuthenticationForm):
