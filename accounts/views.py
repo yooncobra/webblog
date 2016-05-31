e879683c0791601837f536b8ebc41eb0973807da
yooncobra <yooncobra@gmail.com>
2016-05-31 23:05:29 +0900

    회원가입과 동시에, 로그인 처리

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, get_backends, login as auth_login
 from django.contrib.auth.tokens import default_token_generator as token_generator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.encoding import force_text
@@ -13,11 +13,21 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm2(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False
-            user.save()
-            send_signup_confirm_email(request, user)
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+            backend_cls = get_backends()[0].__class__
+            backend_path = backend_cls.__module__ + '.' + backend_cls.__name__
+            # https://github.com/django/django/blob/1.9/django/contrib/auth/__init__.py#L81
+            user.backend = backend_path
+            auth_login(request, user)
+            messages.info(request, '환영합니다. ;)')
+            return redirect(settings.LOGIN_REDIRECT_URL)
+
+            # 회원가입 시에, 이메일 승인
+            # user = form.save(commit=False)
+            # user.is_active = False
+            # user.save()
+            # send_signup_confirm_email(request, user)
+            # return redirect(settings.LOGIN_URL)
     else:
         form = SignupForm2()
     return render(request, 'accounts/signup.html', {
