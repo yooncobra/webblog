972283df26349aa93465445b8d44548a17b35e3c
yooncobra <yooncobra@gmail.com>
2016-05-31 21:04:06 +0900

    custom 모델필드 PhoneField를 구현하고, blog.models.Post에 추가

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,35 @@
+from django import forms
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 
+def min_length_validator(value):
+    if len(value) < 3:
+        raise forms.ValidationError('3글자 이상 입력하세요!')
+
+
+class PhoneField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 11)
+        super(PhoneField, self).__init__(*args, **kwargs)
+        validator = RegexValidator(r'^01[016789]\d{7,8}$',
+                message='휴대폰 번호를 입력해주세요.')
+        self.validators.append(validator)
+
+
 @python_2_unicode_compatible
 class Post(models.Model):
-    title = models.CharField(max_length=100, help_text='포스팅 제목을 100자 이내로 써 주세요.')
+    title = models.CharField(max_length=100,
+            validators=[min_length_validator],
+            help_text='포스팅 제목을 100자 이내로 써 주세요.')
     content = models.TextField()
     photo = models.ImageField(blank=True, help_text='이미지를 첨부하세요.')
+    phone = PhoneField(blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.title
 
