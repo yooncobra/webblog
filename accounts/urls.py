da08d967b624e9e9ba22095c14587c389b15828a
yooncobra <yooncobra@gmail.com>
2016-05-31 22:42:45 +0900

    django.contrib.auth.forms.UserCreationForm 을 활용하여, 회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login
 from accounts.forms import LoginForm
+from accounts import views
 
 urlpatterns = [
+    url(r'^signup/$', views.signup),
     url(r'^login/$', login, kwargs={
         'authentication_form': LoginForm,
     }),
