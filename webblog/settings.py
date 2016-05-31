38e82462a44376d1536da10d76e562213499232f
yooncobra <yooncobra@gmail.com>
2016-05-31 22:28:24 +0900

    accounts 앱을 만들고, settings.INSTALLED_APPS와 프로젝트 urls에 등록

--- webblog/settings.py
+++ webblog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'bootstrap3',
+    'accounts',
     'blog',
     'shop',
 
--- webblog/urls.py
+++ webblog/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^$', 'blog.views.index'),
     url(r'^posts/$', 'blog.views.post_list'),
     url(r'^posts/(?P<pk>\d+)/$', 'blog.views.post_detail'),
