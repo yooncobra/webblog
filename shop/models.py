8893fdba0af99b12a6b5878bf9556d3e853e1ddc
yooncobra <yooncobra@gmail.com>
2016-05-31 11:03:41 +0900

    related_name 중복으로 인한 makemigrations실패상황 재현

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from blog.models import Post
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    content = models.TextField()
 
 # Create your models here.
