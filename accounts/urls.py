9acf3e3192ab90751f4e922414fb1c8cd2df03df
yooncobra <yooncobra@gmail.com>
2016-05-31 22:38:48 +0900

    accounts.forms.LoginForm 폼을 login 뷰에 접목하여, 퀴즈를 맞춰야만 로그인토록 함

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login
+from accounts.forms import LoginForm
 
 urlpatterns = [
-    url(r'^login/$', login),
+    url(r'^login/$', login, kwargs={
+        'authentication_form': LoginForm,
+    }),
 ]
