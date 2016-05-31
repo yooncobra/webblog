8db81f6a39d7d2bee2fa3e26088fb8c9b23fbcd2
yooncobra <yooncobra@gmail.com>
2016-05-31 11:58:09 +0900

    PostForm에 custom form field를 추가 적용// 약관동의

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,9 @@ from blog.models import Post
 
 
 class PostForm(forms.ModelForm):
+    is_agree = forms.BooleanField(label='약관동의',
+            error_messages={'required': '약관에 동의해주셔야, 서비스 이용이가능합니다.'})
+
     class Meta:
         model = Post
         fields = '__all__'
